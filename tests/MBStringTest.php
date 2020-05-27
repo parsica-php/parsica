c24f835db39b0583a7f64c0ade1064651249f928
Mathias Verraes <mathias@verraes.net>
2020-05-27 16:40:48 +0200

    ext-mbstring dependency

---
+++ tests/MBStringTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Mathias\ParserCombinators;
+
+use PHPUnit\Framework\TestCase;
+
+final class MBStringTest extends TestCase
+{
+    /** @test */
+    public function mbstring_should_be_installed()
+    {
+        $this->assertSame("b", mb_substr("abc", 1, 1), "ext-mbstring is not installed.");
+    }
+}
