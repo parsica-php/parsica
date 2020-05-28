66bac0b37067ca4445530622d771e84bd6939002
Mathias Verraes <mathias@verraes.net>
2020-05-28 21:18:53 +0200

    fixed Assert::minLength

--- src/Internal/Assert.php
+++ src/Internal/Assert.php
@@ -21,7 +21,7 @@ final class Assert
      */
     public static function minLength(string $value, int $length, string $message)
     {
-        if($length < mb_strlen($value)) {
+        if(mb_strlen($value) < $length) {
             throw new InvalidArgumentException($message);
         }
     }
