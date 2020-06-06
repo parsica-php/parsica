147903044a11baaed770e9a44d3bcf42a3d39591
Mathias Verraes <mathias@verraes.net>
2020-06-06 17:32:35 +0200

    AlternativeTest for ParseResult

--- tests/ParseResult/AlternativeTest.php
+++ tests/ParseResult/AlternativeTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Mathias\ParserCombinator\ParseResult;
 
 use Mathias\ParserCombinator\PHPUnit\ParserTestCase;
+use function Mathias\ParserCombinator\ParseResult\discard;
 use function Mathias\ParserCombinator\ParseResult\fail;
 use function Mathias\ParserCombinator\ParseResult\succeed;
 
@@ -15,11 +16,20 @@ final class AlternativeTest extends ParserTestCase
         $succeed2 = succeed("S2", "");
         $fail1 = fail("F1", "");
         $fail2 = fail("F2", "");
+        $discard1 = discard("");
+        $discard2 = discard("");
 
-        $this->assertSame($succeed1, $succeed1->alternative($succeed2));
-        $this->assertSame($succeed1, $succeed1->alternative($fail1));
-        $this->assertSame($succeed1, $fail1->alternative($succeed1));
-        $this->assertSame($fail1, $fail1->alternative($fail2));
+        $this->assertStrictlyEquals($succeed1, $succeed1->alternative($succeed2));
+        $this->assertStrictlyEquals($succeed1, $succeed1->alternative($fail1));
+        $this->assertStrictlyEquals($succeed1, $succeed1->alternative($discard1));
+
+        $this->assertStrictlyEquals($succeed1, $fail1->alternative($succeed1));
+        $this->assertStrictlyEquals($fail1, $fail1->alternative($fail2));
+        $this->assertStrictlyEquals($discard1, $fail1->alternative($discard1));
+
+        $this->assertStrictlyEquals($discard1, $discard1->alternative($succeed1));
+        $this->assertStrictlyEquals($discard1, $discard1->alternative($fail1));
+        $this->assertStrictlyEquals($discard1, $discard1->alternative($discard2));
     }
 
 }
