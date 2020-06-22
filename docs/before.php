c1baff0e5bc727f9fbeed3dea7853b93156666be
Mathias Verraes <mathias@verraes.net>
2020-06-22 15:13:54 +0200

    copyright notice

--- docs/before.php
+++ docs/before.php
@@ -1,4 +1,11 @@
-<?php /** @noinspection ALL */
+<?php /**
+ * This file is part of the Parsica library.
+ *
+ * Copyright (c) 2020 Mathias Verraes <mathias@verraes.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */ /** @noinspection ALL */
 declare(strict_types=1);
 // This code is executed by UpToDocs before each code block
 require_once __DIR__ . '/../vendor/autoload.php';
--- tests/Parser/MonadTest.php
+++ tests/Parser/MonadTest.php
@@ -1,4 +1,12 @@
 <?php declare(strict_types=1);
+/**
+ * This file is part of the Parsica library.
+ *
+ * Copyright (c) 2020 Mathias Verraes <mathias@verraes.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Tests\Verraes\Parsica\Parser;
 
