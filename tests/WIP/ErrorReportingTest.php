c7147d834cef33a7dd2a5deedc6e697b47009eab
Mathias Verraes <mathias@verraes.net>
2020-07-02 21:31:00 +0200

    This test indicates the desired error ouput

---
+++ tests/WIP/ErrorReportingTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Parsica library.
+ *
+ * Copyright (c) 2020 Mathias Verraes <mathias@verraes.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Verraes\Parsica\WIP;
+
+use PHPUnit\Framework\TestCase;
+use Verraes\Parsica\Internal\Position;
+use Verraes\Parsica\Internal\StringStream;
+use Verraes\Parsica\PHPUnit\ParserAssertions;
+use function Verraes\Parsica\atLeastOne;
+use function Verraes\Parsica\char;
+
+final class ErrorReportingTest extends TestCase
+{
+    use ParserAssertions;
+
+    /** @test */
+    public function indicate_position_in_error_messages()
+    {
+        $this->markTestSkipped();
+        $parser = char('a');
+        $input = new StringStream("bcd", Position::initial("/path/to/file"));
+        $result = $parser->run($input);
+        $expected = <<<ERROR
+/path/to/file:1:1
+  |
+1 | bcd
+  | ^
+Unexpected 'b'
+Expecting 'a'
+ERROR;
+
+        $this->assertEquals($expected, $result->expected());
+
+    }
+
+
+}
