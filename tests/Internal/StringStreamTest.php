2330006cec9e1ff226529966621785a8cf8ab687
Mathias Verraes <mathias@verraes.net>
2020-07-04 10:29:29 +0200

    StringStreamTest

---
+++ tests/Internal/StringStreamTest.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Parsica library.
+ *
+ * Copyright (c) 2020 Mathias Verraes <mathias@verraes.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Verraes\Parsica\Internal;
+
+use PHPUnit\Framework\TestCase;
+use Verraes\Parsica\Internal\Position;
+use Verraes\Parsica\Internal\StringStream;
+
+final class StringStreamTest extends TestCase
+{
+
+    /** @test */
+    public function take1()
+    {
+        $s = new StringStream("abc");
+        $t = $s->take1();
+        $this->assertEquals("a", $t->chunk());
+        $expectedPosition = new Position("<input>", 1, 2);
+        $expectedStream = new StringStream("bc", $expectedPosition);
+        $this->assertEquals($expectedStream, $t->stream());
+    }
+
+    /** @test */
+    public function takeN()
+    {
+        $s = new StringStream("abcde");
+        $t = $s->takeN(3);
+        $this->assertEquals("abc", $t->chunk());
+        $expectedPosition = new Position("<input>", 1, 4);
+        $expectedStream = new StringStream("de", $expectedPosition);
+        $this->assertEquals($expectedStream, $t->stream());
+    }
+
+    /** @test */
+    public function takeWhile()
+    {
+        $s = new StringStream("abc\nde");
+        $t = $s->takeWhile(fn($c) => $c !== "\n");
+        $this->assertEquals("abc", $t->chunk());
+        $expectedPosition = new Position("<input>", 1, 4);
+        $expectedStream = new StringStream("\nde", $expectedPosition);
+        $this->assertEquals($expectedStream, $t->stream());
+    }
+}
