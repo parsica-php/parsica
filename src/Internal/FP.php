53ecbbb6de23a989676e1071e708f1d0e46c8a15
Mathias Verraes <mathias@verraes.net>
2020-09-26 16:50:36 +0200

    upgraded phpunit xml schema

---
+++ src/Internal/FP.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of the Parsica library.
+ *
+ * Copyright (c) 2020 Mathias Verraes <mathias@verraes.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Verraes\Parsica\Internal\FP;
+
+/**
+ * Swaps the arguments of the callable, returning a callable.
+ *
+ * @internal
+ * @template Ta
+ * @template Tb
+ * @template Tc
+ * @psalm-param callable(Ta, Tb):Tc $f
+ * @psalm-return callable(Tb, Ta):Tc
+ */
+function flip(callable $f): callable
+{
+    /**
+     * @psalm-param Ta $x
+     * @psalm-param Tb $y
+     * @psalm-return Tc
+     */
+    return fn($x, $y) => $f($y, $x);
+}
