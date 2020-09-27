e73ed2185d772b1afa2762c2fb606d09dbb1f29f
Mathias Verraes <mathias@verraes.net>
2020-09-27 11:39:13 +0200

    EXAMPLE Swapping precedence in boolean expressions

--- tests/Examples/BooleanExpressionsTest.php
+++ tests/Examples/BooleanExpressionsTest.php
@@ -26,23 +26,20 @@ final class BooleanExpressionsTest extends TestCase
         $token = fn(Parser $parser) => keepFirst($parser, skipHSpace());
         $parens = fn (Parser $parser): Parser =>  $token(between($token(char('(')), $token(char(')')), $parser));
         $term = fn(): Parser => $token(choice(
-            string("TRUE")->map(fn($v) => new True_()),
-            string("FALSE")->map(fn($v) => new False_()),
+            string("TRUE")->map(fn($v) => new True_),
+            string("FALSE")->map(fn($v) => new False_),
         ));
+        $NOT = operator($token(string("NOT")), fn($v) => new Not_($v));
+        $AND = operator($token(string("AND")), fn($l, $r) => new And_($l, $r));
+        $OR = operator($token(string("OR")), fn($l, $r) => new Or_($l, $r));
 
         $expr = recursive();
         $expr->recurse(expression(
             $parens($expr)->or($term()),
             [
-                prefix(
-                    operator($token(string("NOT")), fn($v) => new Not_($v)),
-                ),
-                leftAssoc(
-                    operator($token(string("AND")), fn($l, $r) => new And_($l, $r)),
-                ),
-                leftAssoc(
-                    operator($token(string("OR")), fn($l, $r) => new Or_($l, $r)),
-                ),
+                prefix($NOT),
+                leftAssoc($AND),
+                leftAssoc($OR),
             ]
         ));
 
@@ -56,6 +53,52 @@ final class BooleanExpressionsTest extends TestCase
                     new And_(
                         new False_(),
                         new False_()
+                    )
+                )
+            );
+        $this->assertParses($input, $parser, $expected);
+
+
+        $parser = $expr->thenEof();
+        $input = "TRUE AND NOT (FALSE OR TRUE AND FALSE)";
+        $expected =
+            new And_(
+                new True_,
+                new Not_(
+                    new Or_(
+                        new False_,
+                        new And_(
+                            new True_,
+                            new False_
+                        )
+                    )
+                )
+            );
+        $this->assertParses($input, $parser, $expected);
+
+        // Now swapping precedence of AND and OR
+        $expr = recursive();
+        $expr->recurse(expression(
+            $parens($expr)->or($term()),
+            [
+                prefix($NOT),
+                leftAssoc($OR),
+                leftAssoc($AND),
+            ]
+        ));
+
+        $parser = $expr->thenEof();
+        $input = "TRUE AND NOT (FALSE OR TRUE AND FALSE)";
+        $expected =
+            new And_(
+                new True_,
+                new Not_(
+                    new And_(
+                        new Or_(
+                            new False_,
+                            new True_
+                        ),
+                        new False_
                     )
                 )
             );
